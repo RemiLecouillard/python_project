94f1cf80a248cccf0757ce57cc3a5bf5b7a3980a
RemiLecouillard <remi@lecouillard.org>
2019-12-26 19:43:29 +0100

    Yeahhhhh
    Co-authored-by: Weber <nicolasweber95@orange.fr>

--- agents.py
+++ agents.py
@@ -88,11 +88,12 @@ class Square_type(Enum):
 
 class State:
 	_map = []
+	_map_size = 0
 	_life = 100
 	_thirst = 100
 	_position = (1,1)
 	_position_dog = (0,0)
-
+	_direction = 0
 		
  #  ______                               _              
  # |  ____|                             (_)             
@@ -108,6 +109,7 @@ class GoalBasedBrain( TortoiseBrain ):
 		self.init_map(grid_size)
 		
 	def init_map(self, grid_size):
+		self._state._map_size = grid_size
 		for i in range(grid_size):
 			self._state._map += [[]]
 			for j in range(grid_size):
@@ -135,6 +137,7 @@ class GoalBasedBrain( TortoiseBrain ):
 		self._state._thirst = sensor.drink_level
 		self._state._position = sensor.tortoise_position
 		self._state._position_dog = self.get_position_of_dog(sensor)
+		self._state._direction = sensor.tortoise_direction
 
 		if sensor.tortoise_direction == 0:
 			self._state._map[self._state._position[0]][self._state._position[1]-1] = self.get_type_of_ahead_square(sensor)
@@ -146,26 +149,86 @@ class GoalBasedBrain( TortoiseBrain ):
 			self._state._map[self._state._position[0] - 1][self._state._position[1]] = self.get_type_of_ahead_square(sensor)
 
 	def get_best_action(self):
+		state = UCS_state()
+		state.init(self._state._position, self._state._map, self._state._map_size)
+		path = self.uc_search(state)
+
+		if self.get_water_cost(self._state._position, path[-1]) < self._state._thirst :
+			return self.perform_meta_action_explore()
+		else :
+			return self.perform_meta_action_drink(path)
+
+	def perform_meta_action_drink(self, path):
+		next_position = path[0]
+		
+		if self._state._map[self._state._position[0]][self._state._position[1]] == Square_type.WATER :
+			return DRINK
+		elif DIRECTIONTABLE[self._state._direction] + self._state._position == next_position :
+			return FORWARD
+		else :
+			return RIGHT
+
+	def perform_meta_action_explore(self ) :
 		pass
 
-	def get_water_cost(self, depart, arrive, sensor):
+		
+
+	# 0 (north), 1 (east), 2 (south), and 3 (west).
+	def get_water_cost(self, depart, arrive):
 		cost = 0
-		if math.abs(depart[0]-arrive[0]) == 0:
-			if math.abs(depart[1]-arrive[1]) > 0:
-				if sensor.tortoise_direction == 0:
+		# même colonne?
+		if math.fabs(depart[1]-arrive[1]) == 0:
+			# aligné en dessous
+			if math.fabs(depart[0]-arrive[0]) > 0:
+				if self._state._direction == 0:
 					cost += 0
-				if sensor.tortoise_direction == 2:
+				if self._state._direction == 2:
 					cost += 2
-				if (sensor.tortoise_direction == 1) or (sensor.tortoise_direction == 3):
+				if (self._state._direction == 1) or (self._state._direction == 3):
 					cost += 1
-			if math.abs(depart[1]-arrive[1]) < 0:
-				if sensor.tortoise_direction == 0:
+			# aligné au dessus
+			if math.fabs(depart[0]-arrive[0]) < 0:
+				if self._state._direction == 0:
 					cost += 2
-				if sensor.tortoise_direction == 2:
+				if self._state._direction == 2:
 					cost += 0
-				if (sensor.tortoise_direction == 1) or (sensor.tortoise_direction == 3):
+				if (self._state._direction == 1) or (self._state._direction == 3):
 					cost += 1
-			
+			# même case
+			if math.fabs(depart[0]-arrive[0]) == 0:
+				return cost
+			cost += math.fabs(depart[0]-arrive[0])
+			return cost
+		# même ligne?
+		elif math.fabs(depart[0]-arrive[0]) == 0:
+			# à droite
+			if math.fabs(depart[1]-arrive[1]) > 0:
+				if self._state._direction == 1:
+					cost += 2
+				if self._state._direction == 3:
+					cost += 0
+				if (self._state._direction == 0) or (self._state._direction == 2):
+					cost += 1
+			# à gauche
+			if math.fabs(depart[1]-arrive[1]) < 0:
+				if self._state._direction == 1:
+					cost += 0
+				if self._state._direction == 3:
+					cost += 2
+				if (self._state._direction == 0) or (self._state._direction == 2):
+					cost += 1
+			# même case
+			if math.fabs(depart[1]-arrive[1]) == 0:
+				return cost
+			cost += math.fabs(depart[1]-arrive[1])
+			return cost
+
+		# pas sur la même ligne/colonne
+		else:
+			cost += math.fabs(depart[1]-arrive[1])
+			cost += math.fabs(depart[0]-arrive[0])
+			cost += 5
+			return cost
 
 	def get_require_water(self, action):
 		pass
@@ -260,14 +323,21 @@ class GoalBasedBrain( TortoiseBrain ):
 
 class UCS_state :
 	_map = []
-	_position
+	_position = (0,0)
+	_size = 0
 
-	def init(self, map, position) :
+	def init(self, map, position, size) :
 		_map = map
 		_position = position
+		_size = size
 
 	def get_successor_states(self) :
-		pass
+		list = []
+		for i in range(0, 4) :
+			next_to = self._position + DIRECTIONTABLE[i]
+			if next_to[0] > 0 and next_to[0] < self._size and  next_to[1] > 0 and next_to[1] < self._size :
+				list.append()
+		return list
 
 	def is_goal_state(self) :
-		pass
+		return self._map[self._position[0]][self._position[1]] == Square_type.WATER
